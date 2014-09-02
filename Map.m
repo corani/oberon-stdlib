5adc68afb910a0a792d24792078e4b44372c8867
Daniel Bos <daniel.bos@microsoft.com>
2014-09-02 13:55:10 +0800

    Refactor Map in terms of a Vector of Pairs

--- Map.m
+++ Map.m
@@ -1,16 +1,15 @@
 MODULE Map;
 
-IMPORT Std;
+IMPORT Std, Vector;
 
-TYPE Node   = POINTER TO NodeRec;
-     NodeRec= RECORD
-                key  : Std.OBJECT;
-                value: Std.OBJECT;
-                next : Node;
+TYPE PAIR*  = POINTER TO PairRec;
+     PairRec= RECORD
+                key   : Std.OBJECT;
+                value : Std.OBJECT
               END;
      MAP*   = POINTER TO MapRec;
      MapRec = RECORD
-                nodes : Node;
+                vec   : VECTOR;
                 len   : INTEGER;
               END;
 
@@ -19,14 +18,14 @@ VAR m : MAP;
 BEGIN
     NEW(m);
     m.len := 0;
-    m.nodes := NIL;
+    m.vec = Vector.Vector();
     RETURN m
 END Map;
 
 PROCEDURE (m: MAP) Clear*();
 BEGIN
     m.len := 0;
-    m.nodes := NIL;
+    m.vec.Clear();
 END Clear;
 
 PROCEDURE (m: MAP) Length*() : INTEGER;
@@ -35,80 +34,69 @@ BEGIN
 END Length;
 
 PROCEDURE (m: MAP) Put*(key: Std.OBJECT; value: Std.OBJECT);
-VAR node, prev : Node;
+VAR i : INTEGER;
+    e : Std.OBJECT;
+    p : PAIR;
 BEGIN
-    prev := NIL;
-    node := m.nodes;
-    WHILE (node # NIL) & (node.key.CompareTo(key) < 0) DO
-        prev := node;
-        node := node.next
-    END;
-    NEW(node);
-    node.key   := key;
-    node.value := value;
-    IF (prev = NIL) THEN
-        (* Insert at start *)
-        node.next := m.nodes;
-        m.nodes   := node;
-        INC(m.len);
-    ELSIF (prev.next = NIL) THEN
-        (* Insert at end *)
-        prev.next := node;
-        INC(m.len);
-    ELSIF (prev.next.key.CompareTo(key) = 0) THEN
-        (* Replace existing *)
-        node.next := prev.next.next;
-        prev.next := node;
-    ELSE
-        (* Insert *)
-        node.next := prev.next;
-        prev.next := node;
-        INC(m.len);
-    END;
-    
+    FOR i := 0 TO m.len - 1 DO
+        e := m.vec.At(i);
+        WITH e: PAIR DO
+            CASE e.key.CompareTo(key) OF
+               -1 : (* Continue *)
+            |   0 : (* Replace *)
+                    e.value := value;
+                    RETURN
+            |   1 : (* Insert *)
+                    NEW(p);
+                    p.key := key;
+                    p.value := value;
+                    m.vec.Insert(i, p);
+                    INC(m.len);
+                    RETURN
+            END
+        END
+    END    
 END Put;
 
 PROCEDURE (m: MAP) Get*(key: Std.OBJECT) : Std.OBJECT;
-VAR node : Node;
+VAR i : INTEGER;
+    e : Std.OBJECT;
 BEGIN
-    node := m.nodes;
-    WHILE (node # NIL) DO
-        IF node.key.CompareTo(key) = 0 THEN
-            RETURN node.value;
-        END;
-        node := node.next
+    FOR i := 0 TO m.len - 1 DO
+        e := m.vec.At(1);
+        WITH e: PAIR DO
+            IF (e.key.CompareTo(key) = 0 THEN
+                RETURN e.value
+            END
+        END
     END;
     RETURN NIL
 END Get;
 
 PROCEDURE (m: MAP) KeyAt*(pos: INTEGER) : Std.OBJECT;
-VAR i    : INTEGER;
-    node : Node;
+VAR i : INTEGER;
+    e : Std.OBJECT;
 BEGIN
-    IF (pos < 0) OR (pos > m.len - 1) THEN
-        RETURN NIL
-    ELSE
-        node := m.nodes;
-        FOR i := 0 TO pos - 1 DO
-            node := node.next
-        END;
-        RETURN node.key
-    END
+    IF (pos >= 0) AND (pos <= m.len - 1) THEN
+        e := m.vec.At(pos);
+        WITH e: PAIR DO
+            RETURN e.key
+        END
+    END;
+    RETURN NIL
 END KeyAt;
 
 PROCEDURE (m: MAP) ValueAt*(pos: INTEGER) : Std.OBJECT;
-VAR i    : INTEGER;
-    node : Node;
+VAR i : INTEGER;
+    e : Std.OBJECT;
 BEGIN
-    IF (pos < 0) OR (pos > m.len - 1) THEN
-        RETURN NIL
-    ELSE
-        node := m.nodes;
-        FOR i := 0 TO pos - 1 DO
-            node := node.next
-        END;
-        RETURN node.value
-    END
+    IF (pos >= 0) AND (pos <= m.len - 1) THEN
+        e := m.vec.At(pos);
+        WITH e: PAIR DO
+            RETURN e.value
+        END
+    END;
+    RETURN NIL
 END ValueAt;
 
 END Map.
